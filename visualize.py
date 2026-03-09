b43503081115210ca4b12f9cee3d2d552f4c5654
nhatle10 <canhnhat922017@gmail.com>
2026-03-09 18:02:22 +0700

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 from PIL import Image
-from IPython.display import display
 
 
 def plot_topk(image_paths, scores):
@@ -9,17 +8,20 @@ def plot_topk(image_paths, scores):
     k = len(image_paths)
     rows = (k + cols - 1) // cols
 
-    fig = plt.figure(figsize=(4*cols,4*rows))
+    plt.figure(figsize=(4*cols,4*rows))
 
     for i,(path,score) in enumerate(zip(image_paths,scores)):
 
         img = Image.open(path).convert("RGB")
 
-        ax = fig.add_subplot(rows, cols, i+1)
-        ax.imshow(img)
-        ax.set_title(f"{score:.3f}")
-        ax.axis("off")
+        plt.subplot(rows,cols,i+1)
+        plt.imshow(img)
+        plt.title(f"{score:.3f}")
+        plt.axis("off")
 
-    display(fig)
+    plt.tight_layout()
 
-    plt.show()
+    save_path = "topk_result.png"
+    plt.savefig(save_path)
+
+    print("Saved visualization:", save_path)
